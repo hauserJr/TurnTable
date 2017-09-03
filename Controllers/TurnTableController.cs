1781b393b8d1118fe7f4e7860149f7586effb78d
hauserJr <free576002@gmail.com>
2017-09-03 23:35:52 +0800

    Initial commit

---
+++ Controllers/TurnTableController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurnTable.Controllers
+{
+    public class TurnTableController : Controller
+    {
+        // GET: TurnTable
+        public ActionResult TurnTable()
+        {
+            return View();
+        }
+
+        public ActionResult RandomChoice()
+        {
+            return Content("1800");
+        }
+        
+        public ActionResult getTable()
+        {
+            int Amount = 6;
+            List <TurnTableItem>  _TurnTableItem = new List<TurnTableItem>();
+            for (int i = 0; i < Amount; i++)
+            {
+                double Min_Angle = ((Math.PI*2) / Amount) * i;
+                double Max_Angle = ((Math.PI*2) / Amount) * (i + 1);
+                _TurnTableItem.Add(new TurnTableItem()
+                {
+                    Chosen_Name = @"Name" + i.ToString(),
+                    Min_Angle = Min_Angle <= 0 ? 0.ToString():Min_Angle.ToString("#.##"),
+                    Max_Angle = Max_Angle <= 0 ? 0.ToString():Max_Angle.ToString("#.##")
+                });
+            }
+            string json_data = JsonConvert.SerializeObject(_TurnTableItem);
+            return Content(json_data);
+        }
+    }
+}
---
+++ Models/TurnTableDefault.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class TurnTableItem
+{
+    /// <summary>
+    /// 被選擇的名稱
+    /// </summary>
+    public string Chosen_Name { get; set; }
+
+    /// <summary>
+    /// 最小角度
+    /// </summary>
+    public string Min_Angle { get; set; }
+
+    /// <summary>
+    /// 最大小杜
+    /// </summary>
+    public string Max_Angle { get; set; }
+
+    /// <summary>
+    /// 最後產生的角度
+    /// </summary>
+    public ChosenValue AngleValue { get; set; }
+}
+
+public class ChosenValue
+{
+    public string AngleValue { get; set; }
+}
